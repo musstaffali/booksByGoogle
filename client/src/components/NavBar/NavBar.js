962677bcaed84a8e728a168e6d54e897a32db381
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-11 16:31:42 -0400

    navBar folder

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import "./NavBar.css";
+
+const NavBar = () => {
+	const [savedBooks, setSavedBooks] = useState([])
+
+	useEffect(() => {
+		loadBooks();
+	}, [savedBooks])
+
+	function loadBooks() {
+		API.getBooks()
+			.then(res =>
+				setSavedBooks(res.data)
+			)
+			.catch(err => console.log(err));
+	};
+
+	return (
+		<Navbar expand="xs" className="fixed-top border-bottom navbardesign">
+
+			<NavbarBrand href="/" className="mr-auto">Google Books 
+			</NavbarBrand>
+
+			<Nav navbar>
+
+				<NavItem>
+					<Link to="/search" className="nav-link">
+						Search
+              		</Link>
+				</NavItem>
+
+				<NavItem>
+					<Link to="/saved" className="nav-link">
+						Saved <span id="savedNum">{savedBooks.length}</span>
+					</Link>
+				</NavItem>
+
+			</Nav>
+		</Navbar>
+	)
+}
+
+export default NavBar;

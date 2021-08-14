2ac46f8e68de5d2bd2457f953587cc8499fe6d83
Mustafa Mustafa <mamusstaffali@gmail.com>
2021-08-14 19:18:04 -0400

    booksController

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,45 +1,47 @@
 const db = require("../models");
 
+
+// Defining methods for the booksController
 module.exports = {
-	findAll: (req, res) => {
-		db.Book
-			.find({})
-			.then(BookData => {
-				res.json(BookData);
-			})
-			.catch(err => console.log(err));
-	},
-	findById: (req, res) => {
-		db.Book
-			.findById(req.params.id)
-			.then(BookData => {
-				res.json(BookData)
-			})
-			.catch(err => console.log(err));
-	},
-	create: (req, res) => {
-		db.Book
-			.create(req.body)
-			.then(BookData => {
-			  res.json(BookData)
-			})
-			.catch(err => console.log(err));
-	},
-	update: (req, res) => {
-		db.Book
-			.findOneAndUpdate({ _id: req.params.id }, req.body)
-			.then(BookData => {
-				res.json(BookData)
-			})
-			.catch(err => console.log(err));
-	},
-	remove: (req, res) => {
-		db.Book
-			.findById({ _id: req.params.id })
-			.then(dbModel => dbModel.remove())
-			.then(BookData => {
-				res.json(BookData)
-			})
-			.catch(err => console.log(err));
-	}
-}
+  
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    console.log("create")
+    db.Book
+      .create(req.body)
+      .then(dbModel => 
+        {console.log(dbModel)
+          res.json(dbModel)
+        }
+        )
+      .catch(
+    err => {
+      console.log(err);
+      res.status(422).json(err)});
+  },
+  update: function(req, res) {
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};

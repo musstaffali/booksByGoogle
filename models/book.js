925990df4431988dcfd1dc413f9079fd82e7cd19
Mustafa Mustafa <mamusstaffali@gmail.com>
2021-08-14 19:34:50 -0400

    code addition

--- models/book.js
+++ models/book.js
@@ -2,23 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    apiId: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    authors: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    image: String,
-    link: String
+  title: { type: String, required: true, unique: true },
+  author: { type: Array, required: true, unique: true },
+  description: String,
+  image: {type: String, trim: true, unique: true},
+  link: {type: String, trim: true, unique: true},
+  date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);

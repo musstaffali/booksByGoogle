38be715f1b4e8afc85845a5694610a74c1b194e9
Mustafa Mustafa <mamusstaffali@gmail.com>
2021-08-14 19:35:47 -0400

    Server.js

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
-// Define middleware for JSON parsing
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Connect to Mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks", {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true
-});
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
-// Define routes here
-app.use(require("./routes"));
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch";
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
-// All other routes sent to the index.html
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Add routes, both API and view
+app.use(routes);
 
 app.listen(PORT, () => {
-	console.log(`App is listening on http://localhost:${PORT} !`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
